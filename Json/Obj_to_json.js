de1f50dd43e6a4c72430eaf8d92e3e97fbf8314e
akshay james <akshayjames98@gmail.com>
2023-08-26 22:48:09 +0530

    updated json age

--- Json/Obj_to_json.js
+++ Json/Obj_to_json.js
@@ -8,13 +8,13 @@ let students = {
 
 }
 
-console.log(students.name);              //now it is an object.
+console.log(students.Age);              //now it is an object.
 
 let res = JSON.stringify(students);
 
 console.log(res);
 
-console.log(res['name']);   //  it is not an object after JSON it will be returned as a string.
+console.log(res['age']);   //  it is not an object after JSON it will be returned as a string.
 
 
 
