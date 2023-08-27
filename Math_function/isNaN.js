10eb77db77c45b72d039cfedbdbe2654b3da11d9
akshay james <akshayjames98@gmail.com>
2023-08-28 00:56:25 +0530

    updated function

--- Math_function/isNaN.js
+++ Math_function/isNaN.js
@@ -1,5 +1,5 @@
-let x = 50;
+let y = 50;
 
-let res = isNaN(x);   //NaN is a fucntion to check it is a number or not ... (NAN- NOT A NUMBER)
+let res = isNaN(y);   //NaN is a fucntion to check it is a number or not ... (NAN- NOT A NUMBER)
 
 console.log(res);
