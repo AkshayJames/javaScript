8e0aa9a0391983d3d0a7b76376269c8820d9d4cd
akshay james <akshayjames98@gmail.com>
2023-09-03 23:11:08 +0530

    array update

--- Array/arraysplice.js
+++ Array/arraysplice.js
@@ -1,7 +1,15 @@
 let fruits=['apple', 'orange', 'grape'];
 
 
-let res=fruits.splice(0,1,'avocado','pineapple','lemonapple');
+let res=fruits.splice(0,1,'avocado1','pineapple','lemonapple');
+
+console.log(res);
+console.log(fruits);
+
+let fruits2=['apple', 'orange', 'grape'];
+
+
+let res2=fruits.splice(0,1,'avocado1','pineapple1','lemonapple1');
 
 console.log(res);
 console.log(fruits);
