499f04b3cda49257d056dcc830bf55f37731a484
akshay james <akshayjames98@gmail.com>
2023-09-03 23:19:26 +0530

    update find

--- Array/find.js
+++ Array/find.js
@@ -11,3 +11,17 @@ console.log(res);
  let res1 =num.flat();
 
 console.log(res);
+
+let num1=[
+    [10,30,40,50,60,],
+    [100,300,40,50,60,],
+    [10,30,400,500,600,],
+];
+    let res2 = num.findIndex((emp)=> emp >30 );  //
+
+console.log(res);
+
+
+ let res3 =num.flat();
+
+console.log(res);
