b147e4b1fa95243fb847086cee43bae4d090e063
akshay james <akshayjames98@gmail.com>
2023-09-03 23:20:25 +0530

    update arrayfillter

--- Array/arrayfilter.js
+++ Array/arrayfilter.js
@@ -4,6 +4,16 @@ let res= fruit.filter((ele) => ele=='orange' || ele  == 'mango');
 
 console.log(res);
 
+let fruit1 = ['orange','apple','pineapple','mango'];
+
+let res1= fruit.filter((ele) => ele=='orange' || ele  == 'mango');
+
+console.log(res1);
+
+
+
+
+
 
 
 
